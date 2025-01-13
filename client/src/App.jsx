e63c3cae50cc4978cc076f04030b82cd6821faf1
harshivkrishna <harshivkrishnam.cse2023@citchennai.net>
2025-01-13 21:39:27 +0530

    modified:   client/src/App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './Pages/Home/Home'
 import ContactPage from './Pages/Contact/ContactPage'
 import AboutPage from './Pages/About/AboutPage'
 import Career from './Pages/Career/Career'
+import Admin from './Pages/Admin/Admin'
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path='/contact' element={<ContactPage/>}></Route>
         <Route path='/about' element={<AboutPage/>}></Route>
         <Route path='/careers' element={<Career/>}></Route>
+        <Route path='/admin' element={<Admin/>}></Route>
       </Routes>
     </>
   )
