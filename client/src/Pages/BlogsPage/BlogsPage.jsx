ec5e21ff5c0b2dcd92ded12b95901ab31731578f
harshivkrishna <harshivkrishnam.cse2023@citchennai.net>
2025-02-09 11:06:09 +0530

    modified:   client/src/Pages/BlogsPage/BlogsPage.jsx

--- client/src/Pages/BlogsPage/BlogsPage.jsx
+++ client/src/Pages/BlogsPage/BlogsPage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../../Components/Navbar/Navbar'
 import './Blogspage.css'
 import Blogs from '../../Components/Blogs/Blogs'
+import Footer from '../../Components/Footer/Footer'
 const BlogsPage = () => {
   return (
     <div>
         <Navbar/>
         <div className='h-28'></div>
         <Blogs/>
+        <Footer/>
     </div>
   )
 }
